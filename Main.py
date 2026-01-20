d58b424cbd96c6acb2464ad4f9fb09b2d33f7620
Olin015 <701027315@gotvdsb.ca>
2026-01-20 08:38:31 -0500

    1

--- Main.py
+++ Main.py
@@ -13,9 +13,9 @@ pygame.font.init()
 pygame.mixer.init(44100,-16,2,512,"none",5)
 
 #setting the screen size
-WIDTH=800
-HEIGHT=600
-screen=pygame.display.set_mode((WIDTH,HEIGHT))
+WIDTH = 800
+HEIGHT = 600
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
 clock=pygame.time.Clock()  #make clock so I can limit the framerate later
 
@@ -50,12 +50,12 @@ while full_game:  #loop to allow the full game to repeat
     screen.blit(loading_txt,(335,280))
     pygame.display.flip()
 
-    snake_speed=5  #how fast the snakes move
+    snake_speed = 5  #how fast the snakes move
 
     #keep track of the location of the extra life
-    life_x=0
-    life_y=900
-    life_time=True
+    life_x = 0
+    life_y = 900
+    life_time = True
 
     secret = [] #keeping track of the secret code
 
@@ -111,9 +111,9 @@ while full_game:  #loop to allow the full game to repeat
 
     caves = []  #cave array
     for c in range(0,4):  #setting up the cave pictures
-        cave=pygame.image.load(os.path.join('assets','cave.png')).convert_alpha()
-        cave=pygame.transform.rotate(cave,(-90))
-        cave=pygame.transform.scale(cave,(200,100))
+        cave = pygame.image.load(os.path.join('assets','cave.png')).convert_alpha()
+        cave = pygame.transform.rotate(cave,(-90))
+        cave = pygame.transform.scale(cave,(200,100))
         caves.append(cave)
 
     snake_rects = []  #snake hitbox array
@@ -122,17 +122,17 @@ while full_game:  #loop to allow the full game to repeat
     gos = []  #array of variables that tell if the snakes are currently going
     snaketails = []  #array of rects to act as the snaketails
     for s in range(0,4):  #setting  up the snake images and hitboxes and controlls for how they move
-        snake=pygame.image.load(os.path.join('assets','snake head.png')).convert_alpha()
-        snake=pygame.transform.scale(snake,(100,150))
-        snake_rect=snake.get_rect()
+        snake = pygame.image.load(os.path.join('assets','snake head.png')).convert_alpha()
+        snake = pygame.transform.scale(snake,(100,150))
+        snake_rect = snake.get_rect()
 
-        snake_rect.topleft=(200*s+50,-50)
+        snake_rect.topleft = (200*s+50,-50)
 
-        snaketail=pygame.Rect(200*s+80,-700,40,700)
+        snaketail = pygame.Rect(200*s+80,-700,40,700)
 
         #variables to help decide when the snakes go
-        go=False
-        go_time=0
+        go = False
+        go_time = 0
 
         go_times.append(go_time)
         gos.append(go)
@@ -142,14 +142,14 @@ while full_game:  #loop to allow the full game to repeat
  
     apples=[]  #array of apples to display extra lives
     for a in range(0,10):  #creating the apples to show how many lives you have from the bottom of your screen
-        apple=pygame.image.load(os.path.join('assets','apple.png'))
-        apple=pygame.transform.scale(apple,(50,50))
+        apple = pygame.image.load(os.path.join('assets','apple.png'))
+        apple = pygame.transform.scale(apple,(50,50))
         apples.append(apple)
 
     #creating the extra life apple that will fall down ocasioanlly
-    extra_life=pygame.image.load(os.path.join('assets', 'apple.png'))
-    extra_life=pygame.transform.scale(extra_life,(50,50))
-    life_rect=extra_life.get_rect()
+    extra_life = pygame.image.load(os.path.join('assets', 'apple.png'))
+    extra_life = pygame.transform.scale(extra_life,(50,50))
+    life_rect = extra_life.get_rect()
     
     def draw():  #function to draw the images
 
@@ -203,44 +203,44 @@ while full_game:  #loop to allow the full game to repeat
             global life_y
             global life_time
             #deciding if the snakes go or not
-            if gos[s]==False and snake_rects[s].y<= -50:
+            if not gos[s] and snake_rects[s].y <= -50:
                 if play or master:
-                    go_times[s]=random.randint(0,600)
-                    if go_times[s]>590:
-                        gos[s]=True
-                    if go_times[s]>=600 and life_time==True and lives<10 and lives>0:
-                        life_y=0
-                        life_x=random.randint(100,700)
-                        life_time=False
+                    go_times[s] = random.randint(0,600)
+                    if go_times[s] > 590:
+                        gos[s] = True
+                    if go_times[s] >= 600 and life_time == True and lives < 10 and lives > 0:
+                        life_y = 0
+                        life_x = random.randint(100,700)
+                        life_time = False
 
 
             #snakes moving foreward and backward
-            if gos[s]==True:
-                snake_rects[s].y+=snake_speed
-                snaketails[s].y+=snake_speed
-            elif snake_rects[s].y>-50 and gos[s]==False:
-                snake_rects[s].y-=snake_speed
-                snaketails[s].y-=snake_speed
+            if gos[s] == True:
+                snake_rects[s].y += snake_speed
+                snaketails[s].y += snake_speed
+            elif snake_rects[s].y > -50 and gos[s] == False:
+                snake_rects[s].y -= snake_speed
+                snaketails[s].y -= snake_speed
 
             #if snakes make it across the whole screen
-            if snake_rects[s].y>=500:
-                gos[s]=False
-                if play==True and master==False:
-                    lives-=1
+            if snake_rects[s].y >= 500:
+                gos[s] = False
+                if play == True and master == False:
+                    lives -= 1
                 you_damaged.play()
 
             mouse_hit=pygame.mouse.get_pressed()
 
             #clicking on the snakes
-            if mouse_hit[0] and snake_rects[s].collidepoint(pygame.mouse.get_pos()) and gos[s]==True and play==True and snake_rects[s].y>100 and mouse_click==False:
+            if mouse_hit[0] and snake_rects[s].collidepoint(pygame.mouse.get_pos()) and gos[s] and play and snake_rects[s].y > 100 and not mouse_click:
                 gos[s]=False
                 score += 1
-                mouse_click=True
+                mouse_click = True
                 clicked.play()
 
             #send snakes back into the holes if you lose the game
-            if play==False:
-                gos[s]=False
+            if not play:
+                gos[s] = False
 
             #speed up snakes
             if score==score_check+10:
@@ -250,15 +250,15 @@ while full_game:  #loop to allow the full game to repeat
 
             #clicking on the extra live
             if mouse_hit[0] and life_rect.collidepoint(pygame.mouse.get_pos()) and life_time==False and lives<10 and lives>0 and mouse_click==False:
-                lives+=1
-                life_y+=900
-                life_time=True
-                mouse_click=True
+                lives += 1
+                life_y += 900
+                life_time = True
+                mouse_click = True
                 life_up.play()
 
             #resetting the mouse click
             if not mouse_hit[0]:
-                mouse_click=False
+                mouse_click = False
 
     while text_scroll:  #create the text scroll
      
@@ -271,7 +271,7 @@ while full_game:  #loop to allow the full game to repeat
         screen.fill(('black'))  #fill the screen with black
 
         #play title music
-        if title_playing == False:
+        if not title_playing:
             title_playing = True
             title_song.play(9,0,0,)
 
@@ -302,37 +302,37 @@ while full_game:  #loop to allow the full game to repeat
         keys=pygame.key.get_pressed()  #checks if each key is being pressed
 
         #secret code
-        if keys[pygame.K_UP] and key_pressed == False:  #if up is entered
+        if keys[pygame.K_UP] and not key_pressed:  #if up is entered
             secret.append('up')
             code_entered+=1
             key_pressed=True
 
-        elif keys[pygame.K_DOWN] and key_pressed == False:  #if down is entered
+        elif keys[pygame.K_DOWN] and not key_pressed:  #if down is entered
             secret.append('down')
             code_entered+=1
             key_pressed=True
 
-        elif keys[pygame.K_LEFT] and key_pressed == False:  #if left is entered
+        elif keys[pygame.K_LEFT] and not key_pressed:  #if left is entered
             secret.append('left')
             code_entered += 1
             key_pressed=True
         
-        elif keys[pygame.K_RIGHT] and key_pressed == False:  #if right is entered
+        elif keys[pygame.K_RIGHT] and not key_pressed:  #if right is entered
             secret.append('right')
             code_entered+=1
             key_pressed=True
      
-        elif keys[pygame.K_b] and key_pressed == False and code_entered == 8:  #if b is entered
+        elif keys[pygame.K_b] and not key_pressed and code_entered == 8:  #if b is entered
             secret.append('b')
             code_entered += 1
             key_pressed=True
       
-        elif keys[pygame.K_a] and key_pressed == False and code_entered == 9:  #if a is entered
+        elif keys[pygame.K_a] and not key_pressed and code_entered == 9:  #if a is entered
             secret.append('a')
             code_entered += 1
             key_pressed = True
      
-        elif any(keys) and key_pressed==False:  #if any key is pressed end the text scroll
+        elif any(keys) and not key_pressed:  #if any key is pressed end the text scroll
             text_scroll=False
             title_song.stop()
    
@@ -365,23 +365,23 @@ while full_game:  #loop to allow the full game to repeat
         keys=pygame.key.get_pressed()
 
         #start the music
-        if audio_playing == False:
+        if not audio_playing:
             audio_playing = True
             requiem.play(9,0,0,)
         
 
         #moving the extra lives
-        life_y+=snake_speed
+        life_y += snake_speed
         life_rect.topleft = (life_x,life_y)
-        if life_y>HEIGHT:
-            life_time=True
+        if life_y > HEIGHT:
+            life_time = True
      
         snake_movement()  #controlling the snakes
         draw()  #draw the images
         
         #when you lose the game
-        if lives<=0 and master==False:
-            play=False
+        if lives <= 0 and not master:
+            play = False
             screen.blit(loss_txt,(335,200))
             screen.blit(end_txt,(255,300))
             screen.blit(quit_txt,(280,400))
@@ -389,10 +389,10 @@ while full_game:  #loop to allow the full game to repeat
                 you_die.play(0,0,0)
                 died_audio_played = True
             if keys[pygame.K_p]:
-                running=False
+                running = False
             elif keys[pygame.K_q]:
-                running=False
-                full_game=False
+                running = False
+                full_game = False
             
 
         #make display appear
